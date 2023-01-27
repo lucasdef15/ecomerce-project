e646b2bbc06a8808f5b34f29b6e1f1db82c0fc05
lucas <lucasff15@hotmail.com>
2023-01-27 16:56:24 -0300

    modified:   cart.html 	renamed:    img/Products/Meu projeto.png -> img/Products/b1000.png 	renamed:    img/Products/f2.png -> img/Products/m1000.png 	new file:   img/Products/products_multiple/b1000n0.png 	new file:   img/Products/products_multiple/m1000n0.png 	new file:   img/Products/products_multiple/m1000n1.png 	new file:   img/Products/products_multiple/m1000n2.png     modified:   index.html 	modified:   scripts/cart.js 	new file:   scripts/products.js 	modified:   scripts/sproduct.js 	modified:   shop.html 	new file:   singlePage/spb1000.html 	renamed:    singlePage/sproduct.html -> singlePage/spm1000.html

---
+++ scripts/products.js
@@ -0,0 +1,27 @@
+let products = [
+    {
+        name: 'Bolsa Femina Florida',
+        tag: 'bolsafeminaflorida',
+        price: 254,
+        inCart: 0
+    },
+    {
+        name: 'Necessaires Varias Estampas',
+        tag: 'necessairesvariasestampas',
+        price: 120,
+        inCart: 0
+    },
+    {
+        name: 'Estojo Estampa Gatos',
+        tag: 'estojoestampagatos',
+        price: 54,
+        inCart: 0
+    },
+    {
+        name: 'Bolsa Feminina Estampada',
+        tag: 'bolsafeminaestampas',
+        price: 174,
+        inCart: 0
+    },
+    
+]
--- scripts/sproduct.js
+++ scripts/sproduct.js
@@ -1,13 +1,107 @@
-const addToCard = document.querySelector('[data-cart-btn]')
+const carts = document.querySelectorAll('.add-cart')
 
-addToCard.addEventListener('click', getInfo)
+carts.forEach((cart, i) => {
+    cart.addEventListener('click', () => {
+        cartNumbers(products[i])
+        totalCoast(products[i])
+    })
+})
 
-function getInfo(){
-    const productName = document.querySelector('[data-product-name]')
-    const productPrice = document.getElementById('product-price')
-    const productValue = document.querySelector('[data-input-value]')
-
-    
-    localStorage.setItem('name', productName.textContent);
-    localStorage.setItem('price', Number(productPrice.dataset.price));        localStorage.setItem('number', productValue.value);
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers')
+    if(cartNumbers){
+        let cartNumbers = document.querySelectorAll('.cart-count')
+        cartNumbers.forEach(cartNumber => {
+            cartNumber.textContent = productNumbers
+        })
+    }
 }
+
+function cartNumbers(product){
+    let productNumbers = localStorage.getItem('cartNumbers')
+    productNumbers = parseInt(productNumbers)
+    if(productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers + 1)
+        let cartCount = document.querySelectorAll('.cart-count')
+        cartCount.forEach(cart =>{
+            cart.textContent = productNumbers + 1
+        })
+    }else{
+        localStorage.setItem('cartNumbers', 1)
+        let cartCount = document.querySelectorAll('.cart-count')
+        cartCount.forEach(cart =>{
+            cart.textContent = 1
+        })
+    }
+    setItems(product)
+}
+
+function setItems(product){
+    let cartItems = localStorage.getItem('productsInCart')
+    cartItems = JSON.parse(cartItems)
+
+    if(cartItems != null){
+
+        if(cartItems[product.tag] == undefined){
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1 
+    }else{
+        product.inCart = 1
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    localStorage.setItem('productsInCart', JSON.stringify(cartItems) )
+}
+
+function totalCoast(product){
+    let cartCost = localStorage.getItem('totalCoast')
+
+    if(cartCost != null){
+        cartCost = parseInt(cartCost)
+        localStorage.setItem('totalCoast', cartCost + product.price)
+    }else{
+        localStorage.setItem('totalCoast', product.price)
+    }
+}
+
+function showNumbersInCart(){
+    let cartNumbers = localStorage.getItem('cartNumbers')
+    if(cartNumbers){
+        document.querySelectorAll('.cart-count').textContent = cartNumbers
+    }
+}
+
+function displayCart(){
+    let cartItems = localStorage.getItem('productsInCart')
+    cartItems = JSON.parse(cartItems)
+    let productContainer = document.querySelector('#products')
+    if( cartItems && productContainer){
+        productContainer.innerHTML = ''
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <tr>
+                <td>
+                    <a><i class="far fa-times-circle"></i></a>
+                </td>
+                <td>
+                    <img src="./img/Products/f1.jpg" alt="">
+                </td>
+                <td>${item.name}</td>
+                <td>$${item.price}.00</td>
+                <td>
+                    <input type="number" value="${item.inCart}">
+                </td>
+                <td>$${item.inCart * item.price}.00</td>
+            </tr>`
+        })
+    }
+
+}
+
+onLoadCartNumbers()
+displayCart()
