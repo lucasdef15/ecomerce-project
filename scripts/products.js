7eafe3d108dee8d14e06921c6eeb64ff5fbd5fdd
lucas <lucasff15@hotmail.com>
2023-02-05 01:13:36 -0300

    modified:   about.html 	modified:   cart.html 	modified:   contact.html 	new file:   img/Logo/My project-1.png 	new file:   img/Logo/sewing-machine-svgrepo-com.png 	modified:   img/Logo/sewing-machine-svgrepo-com.svg 	new file:   img/Logo/short-cut-icon-machine (1).png 	new file:   img/Logo/short-cut-icon-machine-resized.png 	new file:   img/Logo/short-cut-icon-machine.png 	new file:   img/Logo/short-cut-icon-machine.svg 	new file:   img/Logo/short-cut-icon.svg 	renamed:    img/Products/HeroPage.png -> img/Products/Others/HeroPage.png 	renamed:    img/Products/My project-1 (1).png -> img/Products/Others/My project-1 (1).png 	renamed:    img/Products/f10.png -> img/Products/Others/f10.png 	renamed:    img/Products/f11.png -> img/Products/Others/f11.png 	renamed:    img/Products/f1.png -> img/Products/e1001.png 	renamed:    img/Products/f4.png -> img/Products/e1002.png 	renamed:    img/Products/f3.png -> img/Products/e1003.png 	renamed:    img/Products/f14.png -> img/Products/e1004.png 	deleted:    img/Products/f5.png 	renamed:    img/Products/f6.png -> img/Products/l1000.png 	renamed:    img/Products/f18.png -> img/Products/n1001.png 	renamed:    img/Products/f17.png -> img/Products/n1002.png 	renamed:    img/Products/f16.png -> img/Products/n1003.png 	renamed:    img/Products/f15.png -> img/Products/n1004.png 	renamed:    img/Products/f13.png -> img/Products/n1005.png 	renamed:    img/Products/f12.png -> img/Products/n1006.png 	modified:   img/Products/products_multiple/b1000n0.png 	modified:   img/Products/products_multiple/m1000n0.png 	modified:   img/Products/products_multiple/m1000n1.png 	modified:   img/Products/products_multiple/m1000n2.png 	modified:   img/banner/a6.jpg 	deleted:    img/banner/b1.jpg 	deleted:    img/banner/b10.jpg 	deleted:    img/banner/b11.jpg 	deleted:    img/banner/b12.jpg 	deleted:    img/banner/b13.jpg 	deleted:    img/banner/b14.jpg 	deleted:    img/banner/b17.jpg 	deleted:    img/banner/b1r.jpg 	deleted:    img/banner/b2.jpg 	new file:   img/banner/b22.jpg 	new file:   img/banner/b23.jpg 	modified:   img/banner/b3.jpg 	modified:   img/banner/b4.jpg 	deleted:    img/banner/b5.jpg 	deleted:    img/banner/b6.jpg 	deleted:    img/banner/b8.jpg 	deleted:    img/banner/b9.jpg 	deleted:    img/banner/f19.jpg 	deleted:    img/banner/f20.jpg 	new file:   img/flores/flor1.jpg 	new file:   img/flores/flor2.jpg 	new file:   img/flores/flor6.png 	new file:   img/flores/flor7.png 	new file:   img/flores/flor8.png 	new file:   img/flores/flor9.png 	modified:   index.html 	modified:   scripts/cart.js 	modified:   scripts/products.js 	modified:   scripts/sproduct.js 	modified:   shop.html 	new file:   singlePage/spe1000.html 	new file:   singlePage/spn1000.html 	modified:   styles/style.css

--- scripts/products.js
+++ scripts/products.js
@@ -1,27 +1,98 @@
 let products = [
-    {
-        name: 'Bolsa Femina Florida',
-        tag: 'b1000',
-        price: 254,
-        inCart: 0
-    },
-    {
-        name: 'Necessaires Varias Estampas',
-        tag: 'n1000',
-        price: 120,
-        inCart: 0
-    },
-    {
-        name: 'Estojo Estampa Gatos',
-        tag: 'e1000',
-        price: 54,
-        inCart: 0
-    },
-    {
-        name: 'Bolsa Feminina Estampada',
-        tag: 'b1001',
-        price: 174,
-        inCart: 0
-    },
-    
-]
+  {
+    name: "Bolsa Professora Com Repartições",
+    tag: "b1000",
+    price: 170,
+    inCart: 0,
+  },
+  {
+    name: "Necessaires Tamanho P",
+    tag: "n1000",
+    price: 15,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Um Compartimento Branco",
+    tag: "e1000",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Bolsa Professora Com Repartições",
+    tag: "b1001",
+    price: 170,
+    inCart: 0,
+  },
+  {
+    name: "Lancheira Térmica",
+    tag: "l1000",
+    price: 95,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Miney Um Compartimento Vermelho",
+    tag: "e1001",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Mickey Um Compartimento Branco",
+    tag: "e1002",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Mickey Um Compartimento Azul",
+    tag: "e1003",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Mochila Mickey - Bolso 3D Tamanho Grande",
+    tag: "m1000",
+    price: 250,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Estampa Mandala",
+    tag: "n1001",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Estampa Flamingo",
+    tag: "n1002",
+    price: 35,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Com Forro Impermeavél",
+    tag: "n1003",
+    price: 50,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Tecido Sintético",
+    tag: "n1004",
+    price: 20,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Um Compartimento Tecido Sintético",
+    tag: "e1004",
+    price: 20,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Multiuso",
+    tag: "n1005",
+    price: 35,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Animal Print Sintético",
+    tag: "n1006",
+    price: 35,
+    inCart: 0,
+  },
+];
--- scripts/sproduct.js
+++ scripts/sproduct.js
@@ -1,112 +1,119 @@
-const carts = document.querySelectorAll('.add-cart')
-const numbers = document.querySelectorAll('.numbers')
+const carts = document.querySelectorAll(".add-cart");
+const numbers = document.querySelectorAll(".numbers");
 
 carts.forEach((cart, i) => {
-    cart.addEventListener('click', () => {
-        cartNumbers(products[i])
-        totalCoast(products[i])
-    })
-})
+  cart.addEventListener("click", () => {
+    cartNumbers(products[i]);
+    totalCoast(products[i]);
+  });
+});
 
 function sendMessage() {
-    let cartItems = localStorage.getItem('productsInCart')
-    let formatedProductList = []
-    let productList
-    let message
+  let cartItems = localStorage.getItem("productsInCart");
+  let formatedProductList = [];
+  let productList;
+  let message;
 
-    cartItems = JSON.parse(cartItems)
-    
-    Object.values(cartItems).forEach(product => {
-        formatedProductList.push((`${product.name}, Quantidade: ${product.inCart}`))
-        
-    })
-    productList = formatedProductList.toString()
-    checkProductListSize = () =>{
-        if(formatedProductList.length > 1){
-            message = `Olá, estou interessado em compras esses produtos: 
-            ${productList}`
-            
-        }else{
-            message = `Olá, estou interessado em compras esse produto: 
-            ${productList}`
-        }
+  cartItems = JSON.parse(cartItems);
+
+  Object.values(cartItems).map((product) => {
+    formatedProductList.push({
+      name: product.name,
+      quantity: product.inCart,
+    });
+  });
+
+  console.log(formatedProductList);
+
+  productList = formatedProductList.toString();
+  checkProductListSize = () => {
+    if (formatedProductList.length > 1) {
+      message = `Olá, estou interessado em compras esses produtos: `;
+      formatedProductList.forEach((product) => {
+        message += `${product.name}, Quantidade: ${product.quantity}\n`;
+      });
+    } else {
+      message = `Olá, estou interessado em comprar esse produto: `;
+      formatedProductList.forEach((product) => {
+        message += `${product.name}, Quantidade: ${product.quantity}\n`;
+      });
     }
-    checkProductListSize()
-    var whatsappUrl = "https://api.whatsapp.com/send?phone=+5519995723855&text=" + encodeURIComponent(message);
-    window.open(whatsappUrl);
-
+  };
+  checkProductListSize();
+  var whatsappUrl =
+    "https://api.whatsapp.com/send?phone=+5519995723855&text=" +
+    encodeURIComponent(message);
+  window.open(whatsappUrl);
 }
-function onLoadCartNumbers(){
-    let productNumbers = localStorage.getItem('cartNumbers')
-    if(cartNumbers){
-        let cartNumbers = document.querySelectorAll('.cart-count')
-        cartNumbers.forEach(cartNumber => {
-            cartNumber.textContent = productNumbers
-        })
-    }
-}
-
-function cartNumbers(product){
-    let productNumbers = localStorage.getItem('cartNumbers')
-    productNumbers = parseInt(productNumbers)
-    if(productNumbers){
-        localStorage.setItem('cartNumbers', productNumbers + 1)
-        let cartCount = document.querySelectorAll('.cart-count')
-        cartCount.forEach(cart =>{
-            cart.textContent = productNumbers + 1
-        })
-    }else{
-        localStorage.setItem('cartNumbers', 1)
-        let cartCount = document.querySelectorAll('.cart-count')
-        cartCount.forEach(cart =>{
-            cart.textContent = 1
-        })
-    }
-    setItems(product)
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  if (cartNumbers) {
+    let cartNumbers = document.querySelectorAll(".cart-count");
+    cartNumbers.forEach((cartNumber) => {
+      cartNumber.textContent = productNumbers;
+    });
+  }
 }
 
-function setItems(product){
-    let cartItems = localStorage.getItem('productsInCart')
-    cartItems = JSON.parse(cartItems)
-
-    if(cartItems != null){
-        if(cartItems[product.tag] == undefined){
-            cartItems = {
-                ...cartItems,
-                [product.tag]: product 
-            }
-        }
-        cartItems[product.tag].inCart += 1 
-    }else{
-        product.inCart = 1
-        cartItems = {
-            [product.tag]: product
-        }
-    }
-    localStorage.setItem('productsInCart', JSON.stringify(cartItems) )
+function cartNumbers(product) {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  productNumbers = parseInt(productNumbers);
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    let cartCount = document.querySelectorAll(".cart-count");
+    cartCount.forEach((cart) => {
+      cart.textContent = productNumbers + 1;
+    });
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    let cartCount = document.querySelectorAll(".cart-count");
+    cartCount.forEach((cart) => {
+      cart.textContent = 1;
+    });
+  }
+  setItems(product);
 }
 
-function totalCoast(product){
-    let cartCost = localStorage.getItem('totalCoast')
+function setItems(product) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
 
-    if(cartCost != null){
-        cartCost = parseInt(cartCost)
-        localStorage.setItem('totalCoast', cartCost + product.price)
-    }else{
-        localStorage.setItem('totalCoast', product.price)
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
     }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  }
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
+function totalCoast(product) {
+  let cartCost = localStorage.getItem("totalCoast");
 
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCoast", cartCost + product.price);
+  } else {
+    localStorage.setItem("totalCoast", product.price);
+  }
+}
 
-function displayCart(){
-    let cartItems = localStorage.getItem('productsInCart')
-    cartItems = JSON.parse(cartItems)
-    let productContainer = document.querySelector('#products')
-    if( cartItems && productContainer){
-        productContainer.innerHTML = ''
-        Object.values(cartItems).map(item => {
-            productContainer.innerHTML += `
+function displayCart() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let productContainer = document.querySelector("#products");
+  if (cartItems && productContainer) {
+    productContainer.innerHTML = "";
+    Object.values(cartItems).map((item) => {
+      productContainer.innerHTML += `
             <tr id = 'product_${item.tag}'>
                 <td>
                     <a onclick="removeItems('${item.tag}')">
@@ -119,117 +126,130 @@ function displayCart(){
                 <td>${item.name}</td>
                 <td>R$${item.price},00</td>
                 <td>
-                    <input onchange="changeCartNumber('${item.tag}')" class="numbers" type="number" value="${item.inCart}">
+                    <input onchange="changeCartNumber('${
+                      item.tag
+                    }')" class="numbers" type="number" value="${item.inCart}">
                 </td>
                 <td>R$${item.inCart * item.price},00</td>
-            </tr>`
-        })
+            </tr>`;
+    });
+  }
+}
+
+function removeItems(productTag) {
+  let element = document.getElementById(`product_${productTag}`);
+  let cartItems = localStorage.getItem("productsInCart");
+  let numbersInCart = localStorage.getItem("cartNumbers");
+  let totalCoasts = localStorage.getItem("totalCoast");
+  let total = 0;
+  let totalInCart = 0;
+
+  cartItems = JSON.parse(cartItems);
+
+  Object.values(cartItems).forEach((item) => {
+    if (item.tag == productTag) {
+      total = item.inCart * item.price;
+      totalInCart = item.inCart;
+      item.inCart -= 1;
+      delete cartItems[item.tag];
     }
+  });
+
+  localStorage.setItem("totalCoast", parseInt(totalCoasts) - total);
+  localStorage.setItem(
+    "cartNumbers",
+    parseInt(numbersInCart) - parseInt(totalInCart)
+  );
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+  element.remove(element);
+
+  calculateTotals();
+  onLoadCartNumbers();
 }
 
-function removeItems(productTag){
-    let element = document.getElementById(`product_${productTag}`)
-    let cartItems = localStorage.getItem('productsInCart')
-    let numbersInCart = localStorage.getItem('cartNumbers')
-    let totalCoasts = localStorage.getItem('totalCoast')
-    let total = 0
-    let totalInCart = 0
-    
-    cartItems = JSON.parse(cartItems)
+function changeCartNumber(productTag) {
+  let element = document.getElementById(`product_${productTag}`);
+  let cartItems = localStorage.getItem("productsInCart");
+  let numbersInCart = localStorage.getItem("cartNumbers");
+  let totalCoasts = localStorage.getItem("totalCoast");
+  let rawString = element.children[5].textContent;
+  let rawNumber = rawString
+    .replace(/([R$,])/g, "")
+    .slice(0, rawString.length - 5);
+  let productPrice = 0;
+  let productArray = [];
+  let productList = {};
 
-    Object.values(cartItems).forEach(item =>{
-        if(item.tag == productTag){
-                total = item.inCart * item.price
-                totalInCart = item.inCart
-                item.inCart -= 1
-                delete cartItems[item.tag]
-            }
-    })
-    
-    localStorage.setItem('totalCoast', parseInt(totalCoasts) - total)
-    localStorage.setItem('cartNumbers', parseInt(numbersInCart) - parseInt(totalInCart))
-    localStorage.setItem('productsInCart', JSON.stringify(cartItems))
-    element.remove(element)
-    
-    calculateTotals()
-    onLoadCartNumbers()
-}
+  cartItems = JSON.parse(cartItems);
 
+  Object.values(cartItems).map((product) => {
+    productArray.push(
+      (productList = {
+        name: product.name,
+        quantity: product.inCart,
+      })
+    );
+  });
+  console.log(productArray);
 
-
-
-function changeCartNumber(productTag){
-    let element = document.getElementById(`product_${productTag}`)
-    let cartItems = localStorage.getItem('productsInCart')
-    let numbersInCart = localStorage.getItem('cartNumbers')
-    let totalCoasts = localStorage.getItem('totalCoast')
-    let rawString = element.children[5].textContent
-    let rawNumber = rawString.replace(/([R$,])/g, '').slice(0,rawString.length - 5)
-    let productPrice = 0
-    let productArray = []
-    let productList = {}
-
-    cartItems = JSON.parse(cartItems)
-
-    Object.values(cartItems).map(product => {
-            productArray.push( productList = {
-                name: product.name,
-                quantity: product.inCart
+  Object.values(cartItems).forEach((product) => {
+    if (product.tag == productTag) {
+      productArray.push(
+        (productList = {
+          name: product.name,
+          quantity: product.inCart,
         })
-    })
-    console.log(productArray)
+      );
 
+      if (product.inCart < element.children[4].lastElementChild.valueAsNumber) {
+        productPrice = product.price;
+        product.inCart += 1;
 
-    Object.values(cartItems).forEach(product => {
-        if(product.tag == productTag){
+        localStorage.setItem("cartNumbers", parseInt(numbersInCart) + 1);
+        localStorage.setItem(
+          "totalCoast",
+          parseInt(totalCoasts) + productPrice
+        );
+        localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+        rawNumber = Number(rawNumber) + Number(productPrice);
+        element.children[5].textContent = `R$${Number(rawNumber).toFixed(2)}`;
+        onLoadCartNumbers();
+        calculateTotals();
+      } else if (
+        product.inCart > element.children[4].lastElementChild.valueAsNumber &&
+        element.children[4].lastElementChild.valueAsNumber !== 0
+      ) {
+        productPrice = product.price;
+        product.inCart -= 1;
 
-            productArray.push( productList = {
-                name: product.name,
-                quantity: product.inCart
-            })
-
-            
-
-            if(product.inCart < element.children[4].lastElementChild.valueAsNumber){
-                productPrice = product.price
-                product.inCart += 1
-
-                localStorage.setItem('cartNumbers', parseInt(numbersInCart) + 1)
-                localStorage.setItem('totalCoast', parseInt(totalCoasts) + productPrice)
-                localStorage.setItem('productsInCart', JSON.stringify(cartItems))
-                rawNumber = Number(rawNumber) + Number(productPrice)
-                element.children[5].textContent = `R$${Number(rawNumber).toFixed(2)}`
-                onLoadCartNumbers()
-                calculateTotals()
-            }else if(product.inCart > element.children[4].lastElementChild.valueAsNumber && element.children[4].lastElementChild.valueAsNumber !== 0){
-                productPrice = product.price
-                product.inCart -= 1
-
-                localStorage.setItem('cartNumbers', parseInt(numbersInCart) - 1)
-                localStorage.setItem('totalCoast', parseInt(totalCoasts) - productPrice)
-                localStorage.setItem('productsInCart', JSON.stringify(cartItems))
-                rawNumber = Number(rawNumber) - Number(productPrice)
-                element.children[5].textContent = `R$${Number(rawNumber).toFixed(2)}`
-                onLoadCartNumbers()
-                calculateTotals()
-            }else{
-                removeItems(productTag)
-            }
-        }
-    })
+        localStorage.setItem("cartNumbers", parseInt(numbersInCart) - 1);
+        localStorage.setItem(
+          "totalCoast",
+          parseInt(totalCoasts) - productPrice
+        );
+        localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+        rawNumber = Number(rawNumber) - Number(productPrice);
+        element.children[5].textContent = `R$${Number(rawNumber).toFixed(2)}`;
+        onLoadCartNumbers();
+        calculateTotals();
+      } else {
+        removeItems(productTag);
+      }
+    }
+  });
 }
 
-function calculateTotals(){
-    let totalCoasts = localStorage.getItem('totalCoast')
-    let subTotalContainer = document.querySelector('#total-data')
-    let cartItems = localStorage.getItem('productsInCart')
+function calculateTotals() {
+  let totalCoasts = localStorage.getItem("totalCoast");
+  let subTotalContainer = document.querySelector("#total-data");
+  let cartItems = localStorage.getItem("productsInCart");
 
-    cartItems = JSON.parse(cartItems)
+  cartItems = JSON.parse(cartItems);
 
-    if(subTotalContainer){
-        subTotalContainer.innerHTML = ''
-        Object.values(cartItems).map(item => {
-            subTotalContainer.innerHTML = `
+  if (subTotalContainer) {
+    subTotalContainer.innerHTML = "";
+    Object.values(cartItems).map((item) => {
+      subTotalContainer.innerHTML = `
             <tr>
                 <td>Cart Subtotal</td>
                 <td>R$ ${totalCoasts},00</td>
@@ -242,13 +262,11 @@ function calculateTotals(){
                 <td><strong>Total</strong></td>
                 <td><strong>R$ ${totalCoasts},00</strong></td>
             </tr>
-            `
-        })
-    }
+            `;
+    });
+  }
 }
 
-
-
-calculateTotals()
-onLoadCartNumbers()
-displayCart()
+calculateTotals();
+onLoadCartNumbers();
+displayCart();
